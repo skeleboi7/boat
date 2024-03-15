f361620e92a3cbaa256a647221543fd0985e154d
skeleboi7 <804933@sylvaniastudents.org>
2024-03-15 10:33:56 -0400

    good

---
+++ PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+public float horizontalInput;
+public float speed = 10.0f;
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+    }
+}
